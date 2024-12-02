0a737d4901804c7ed4f74d0a5973f492c292385e
Jair <jaedhc91@hotmail.com>
2024-12-02 15:27:58 -0600

    Added start

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Cargar las variables de entorno según el modo
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      host: env.VITE_HOST || 'localhost',
+      port: parseInt(env.VITE_PORT, 10) || 5173,
+    },
+  };
+});
